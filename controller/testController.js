5cbc53b570ec642e0f7895db31b6312c99b0e33e
Joy Kanyawee <joy.kanyawee1997@gmail.com>
2019-03-31 10:38:00 +0700

    add api

--- controller/testController.js
+++ controller/testController.js
@@ -9,14 +9,37 @@ let responseObject = {
 }
 
 router.get("/getData",  (req, res) => {
-    let result = req.query.key1 * req.query.key2;
-    res.send({ result: result })
+    let result = Number(req.query.key1) + Number(req.query.key2);
+    res.send({ sum: result })
+});
+
+router.get("/getTriangle",  (req, res) => {
+    let result = (1/2)*Number(req.query.base) * Number(req.query.height);
+    res.send({ triangleArea: result })
+});
+
+router.get("/getBMI",  (req, res) => {
+    let result = (req.query.weight)/((req.query.height)/100*(req.query.height)/100);
+    let msg;
+    if(result>25){
+        msg = "Fat"
+    }else if(result==25){
+        msg = "Normal"
+    }else if(result<25){
+        msg = "thin"
+    }
+    result = Number(result.toFixed(2));
+    res.send({ BMI : result , msg: msg })
 });
 /*
 Request
 * */
 router.post("/postData",async  (req, res) => {
-    if(!req.body.key5){
+    responseObject = {
+        msg: "",
+        result: ""
+    }
+    if(!req.body.key){
         responseObject.msg = "Bad request";
     }else{
         responseObject.msg = "success";
